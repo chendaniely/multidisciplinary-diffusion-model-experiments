b8def004590e791cb4730ffdcf11a52caf1998e4
Daniel Chen <Maveri2k@yahoo.com>
2014-10-16 14:10:53 -0400

    new .in file uses a capital 'I' Infl.ex

--- experiments/02-lens/main.py
+++ experiments/02-lens/main.py
@@ -59,7 +59,7 @@ def random_select_and_update(network_of_agents):
         lens_in_file_dir = here + '/' + './Test2.in'
 
         agent_ex_file_dir = here + '/' + './AgentState.ex'
-        infl_ex_file_dir = here + '/' + './infl.ex'
+        infl_ex_file_dir = here + '/' + './Infl.ex'
         agent_state_out_file_dir = here + '/' + './AgentState.out'
 
         selected_agent.update_agent_state('default',

75f1883d22937e17c4ff646e7d6dc268c3b00d6c
Daniel Chen <Maveri2k@yahoo.com>
2015-01-24 20:35:15 -0500

    added difftime parser for timing function

--- src/R/helper.R
+++ src/R/helper.R
@@ -1,1 +1,7 @@
+print_difftime_prompt <- function(str_what_did_you_time, diff_time, sep=':'){
+    parse_time <- unclass(diff_time)[1]
+    parse_units <- attr(unclass(diff_time), 'units')
+    prompt_string <- sprintf('%s took: %s %s', str_what_did_you_time, parse_time, parse_units)
+    cat(prompt_string, '\n')
+    # return(prompt_string)
 }

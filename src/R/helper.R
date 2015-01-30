b47b0de8514ffa3cc862193a8ab992535d35b1cf
Daniel Chen <Maveri2k@yahoo.com>
2015-01-29 22:07:29 -0500

    added helper function that can load a dataset given suffix
    uses the base name from config and loads the file based on the suffix

--- src/R/helper.R
+++ src/R/helper.R
@@ -5,3 +5,14 @@ print_difftime_prompt <- function(str_what_did_you_time, diff_time, sep=':'){
     cat(prompt_string, '\n')
     # return(prompt_string)
 }
+
+load_file <- function(file_suffix){
+    print(sprintf('load %s', file_suffix))
+    file_path <- paste0(config_batch_folder_path, file_suffix)
+
+    strt <- Sys.time()
+    # load list_stacked_df
+    load(file = file_path, envir = globalenv())
+    print_difftime_prompt(sprintf('load %s', file_suffix),
+                          diff_time = Sys.time() - strt)
+}

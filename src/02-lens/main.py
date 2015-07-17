54a717d1a7d83d866d04d7c5972990028241d0d6
Daniel Chen <Maveri2k@yahoo.com>
2015-07-17 15:11:30 -0400

    network config params are in separate sections

--- src/02-lens/main.py
+++ src/02-lens/main.py
@@ -276,7 +276,7 @@ def main():
 
     network_type = config.get('NetworkParameters', 'GraphGenerator')
     if network_type == 'barabasi_albert_graph':
-        m = config.getint('NetworkParameters', 'm')
+        m = config.getint('BarabasiAlbertGraph', 'm')
         logger1.debug('Number of edges to attach from a new node to existing'
                       'nodes: %s', str(m))
         my_network = mann.network.BidirectionalBarabasiAlbertGraph(n, m)
@@ -285,7 +285,7 @@ def main():
         p = config.getfloat('WattsStrogatzGraph', 'p')
         my_network = mann.network.WattsStrogatzGraph(n, k, p)
     elif network_type == 'fast_gnp_random_graph':
-        p = config.getfloat('NetworkParameters', 'ProbEdgeCreation')
+        p = config.getfloat('DirectedFastGNPRandomGraph', 'ProbEdgeCreation')
         logger1.debug('Probablity for edge creation: %s', str(p))
         my_network = mann.network.DirectedFastGNPRandomGraph(n, p)
     else:
